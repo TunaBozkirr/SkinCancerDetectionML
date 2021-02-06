39e58db5424db12d2798ee1028a512eed99fcf38
Suleyman Bozkir <34109169+TunaBozkirr@users.noreply.github.com>
2021-02-06 11:55:04 +0300

    Update and rename feature.py to HsvDeger.py

---
+++ HsvDeger.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+
+
+def karart(image):   
+    img = image
+        
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+        
+    deger_max = v.max()
+        
+    thres = 120
+    if( deger_max > thres ):
+                
+        #v[renk] = ( v[renk]*120 / deger_max )
+        x = deger_max - 120
+        #x = int(deger_max /2)
+        
+        buyuk_deger_max = v >  thres
+
+        v[buyuk_deger_max] -= x
+        
+    renk1=  h >= 160
+    renk2 = h < 182
+    renk = np.logical_and(renk1, renk2)
+    
+    deger = h[renk]
+    if( deger.size != 0 ):
+        deger_max = deger.max()
+        
+        # temp = v > 120
+        thres = 170
+        if( deger_max > thres ):
+            #v[renk] = ( v[renk]*120 / deger_max )
+            x = deger_max - 18
+            #x = int(deger_max /2)
+        
+            buyuk_deger_max = h >  thres
+
+            h[buyuk_deger_max] -= x
+               
+        
+          
+    final_hsv = cv2.merge((h, s, v))
+    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    
+    return img
